a50fac0c2909894184c5b42a53cd1a534b97b798
Devansh Sharma <devansh.sharma@thoughtworks.com>
2021-09-01 17:41:43 +0530

    [Devansh/Sidharth] Removed commands to kill server

--- run_e2e_tests.sh
+++ run_e2e_tests.sh
@@ -7,10 +7,3 @@ TEST_URL=$3
 API_KEY=$API_KEY CLIENT_ID=$CLIENT_ID npm run dev &
 sleep 30
 TEST_URL=$TEST_URL npm run end_to_end_test
-
-ps -eo 'pgid pid command'
-ps fj
-PID=$(jobs -p)
-echo $PID
-kill -- -$(ps -o pgid= $PID | grep -o [0-9]*)
-ps fj
