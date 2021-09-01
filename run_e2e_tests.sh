6868d8a4365739cac8dbfd8087398901f0436d7f
Devansh Sharma <devansh.sharma@thoughtworks.com>
2021-09-01 17:08:04 +0530

    [Devansh/Sidharth] Added TEST_URL

--- run_e2e_tests.sh
+++ run_e2e_tests.sh
@@ -2,10 +2,11 @@
 
 API_KEY=$1
 CLIENT_ID=$2
+TEST_URL=$3
 
 API_KEY=$API_KEY CLIENT_ID=$CLIENT_ID npm run dev &
 sleep 30
-npm run end_to_end_test
+TEST_URL=$TEST_URL npm run end_to_end_test
 
 ps -eo 'pgid pid command'
 ps fj
