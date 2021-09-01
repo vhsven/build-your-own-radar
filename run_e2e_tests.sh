ca6ebe248d87fff7340f63236d27fcb78fb19b93
Devansh Sharma <devansh.sharma@thoughtworks.com>
2021-09-01 17:34:03 +0530

    [Devansh/Sidharth] Fixed TEST_URL value

--- run_e2e_tests.sh
+++ run_e2e_tests.sh
@@ -6,7 +6,7 @@ TEST_URL=$3
 
 API_KEY=$API_KEY CLIENT_ID=$CLIENT_ID npm run dev &
 sleep 30
-TEST_URL_LOCAL=$TEST_URL npm run end_to_end_test
+TEST_URL=$TEST_URL npm run end_to_end_test
 
 ps -eo 'pgid pid command'
 ps fj
