bc36d935fb6a22d68fb6ce58bffeb3baa6fe6f7e
Devansh Sharma <devansh.sharma@thoughtworks.com>
2021-09-01 15:35:47 +0530

    [Devansh/Sidharth] Moved commands to script file

---
+++ run_e2e_tests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+API_KEY=$1
+CLIENT_ID=$2
+
+API_KEY=$API_KEY CLIENT_ID=$CLIENT_ID npm run dev &
+sleep 30
+npm run end_to_end_test
+
+ps -eo 'pgid pid command'
+ps fj
+PID=$(jobs -p)
+echo $PID
+kill -- -$(ps -o pgid= $PID | grep -o [0-9]*)
+ps fj
