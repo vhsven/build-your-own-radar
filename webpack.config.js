32fdc87fc9b8f91d7b21806dd3854be3c8af7bea
vhsven <vhsven@msn.com>
2022-04-01 22:12:50 +0200

    Change webpack publicpath to auto

--- webpack.config.js
+++ webpack.config.js
@@ -60,7 +60,7 @@ module.exports = {
 
   output: {
     path: buildPath,
-    publicPath: '/',
+    publicPath: 'auto',
     filename: '[name].[contenthash].js',
     assetModuleFilename: 'images/[name][ext]',
   },
